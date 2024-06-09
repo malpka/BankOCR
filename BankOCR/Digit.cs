b9a1db02cc8e3a8988fb07b21eee95b9c60ef581
Piotr Piwowarczyk <malpka@gmail.com>
2024-06-09 17:22:36 +0200

    Added Digit class with consts

---
+++ BankOCR/Digit.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOCR
+{
+    public class Digit
+    {
+        public const string DIGIT_0 =
+            " _ " +
+            "| |" +
+            "|_|";
+        public const string DIGIT_1 =
+            "   " +
+            "  |" +
+            "  |";
+        public const string DIGIT_2 =
+            " _ " +
+            " _|" +
+            "|_ ";
+        public const string DIGIT_3 =
+            " _ " +
+            " _|" +
+            " _|";
+        public const string DIGIT_4 =
+            "   " +
+            "|_|" +
+            "  |";
+        public const string DIGIT_5 =
+            " _ " +
+            "|_ " +
+            " _|";
+        public const string DIGIT_6 =
+            " _ " +
+            "|_ " +
+            "|_|";
+        public const string DIGIT_7 =
+            " _ " +
+            "  |" +
+            "  |";
+        public const string DIGIT_8 =
+            " _ " +
+            "|_|" +
+            "|_|";
+        public const string DIGIT_9 =
+            " _ " +
+            "|_|" +
+            " _|";
+    }
+}
